be4dd1f0c963ab809b9ffe995f36e8be1a769565
unknown <crabbypatty21>
2025-12-22 19:15:50 +0800

    design of dashboard, SP, OOO

--- src/pages/admin/OneOnOne.jsx
+++ src/pages/admin/OneOnOne.jsx
@@ -149,7 +149,7 @@ const OneOnOne = () => {
         {level === "services" && (
           <>
             <div className="ooo-header">
-              <div className="000-title">
+              <div className="ooo-title">
                 <h1>ONE-ON-ONE SERVICES</h1>
                 <p className="ooo-subtitle">Manage parent accounts and student registration</p>
               </div>
