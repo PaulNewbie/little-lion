86946fee16f30f054c24cdc087ab116b35e3cc67
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-01-14 19:09:49 +0800

    set up the admin concern

--- src/pages/admin/Concerns.jsx
+++ src/pages/admin/Concerns.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { useAuth } from '../../hooks/useAuth';
 import AdminSidebar from '../../components/sidebar/AdminSidebar';
 
-
+import './css/Concerns.css';
 
 const Concerns = () => {
   return (
-    <>
-    <AdminSidebar />
+    <div className="ac-page-wrapper">
+      <AdminSidebar />
 
-    <div> Admin Concerns Page</div>
-    </>
+      <div> Admin Concerns Page</div>
+    </div>
   );
 }
 
