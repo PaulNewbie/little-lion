15e9bbe02d0fa31ccc9b6184df782ea95e0277dd
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-01-14 13:34:29 +0800

    set up sidebar for concerns

--- src/pages/admin/Concerns.jsx
+++ src/pages/admin/Concerns.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import AdminSidebar from '../../components/sidebar/AdminSidebar';
+
+
 
 const Concerns = () => {
   return (
+    <>
+    <AdminSidebar />
+
     <div> Admin Concerns Page</div>
+    </>
   );
 }
 
